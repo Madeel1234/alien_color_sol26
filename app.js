7befe680ec5a25cbbc6d91334387af73c1baf145
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 17:22:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+// Alien color # 2: choose a color for an alien as you did in Exercise 25, and write an if else chain.
+// If the alien color is green print a statement that the player just earned 5 points for shootin the alien.
+// If the alien color is not green, print a statement that the player just earned 10 points.
+//Write one version of this program that runs the if block and another that runs the else block.
+//Task # 26
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points for shoooting the alien.");
+}
+else {
+    console.log("player just earned 10 points");
+}
+export {};
